58b196638bc15317c9596a310a4e706483f65693
m0wgli <rodiontiutiunyk@gmail.com>
2023-10-02 20:04:10 +0300

    new commit

--- hw_10_project/utils/add_quotes_to_mongo.py
+++ hw_10_project/utils/add_quotes_to_mongo.py
@@ -1,10 +1,13 @@
 import json
 import configparser
 from urllib.parse import quote_plus
+import logging
 
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
+
+logging.basicConfig(filename='app.log', level=logging.ERROR)  
 
 client = MongoClient('mongodb://localhost') 
 
@@ -15,11 +18,20 @@ with open('quotes.json', 'r', encoding='utf-8') as fd:
 
 
 for quote in quotes:
-    author = db.author.find_one({'fullname': quote['author']})
-    if author:
-        db.quote.insert_one({
-            'quote': quote['quote'],
-            'tags': quote['tags'],
-            'author': ObjectId(author['_id'])
+    try:
+        author = db.authors.find_one({'fullname': quote['author']})
+        if author:
+            print(f"Found author: {quote['author']}")
+            db.quotes.insert_one({
+                'quote': quote['quote'],
+                'tags': quote['tags'],
+                'author': ObjectId(author['_id'])
+            })
+        else:
+            print(f"Author not found for: {quote['author']}")
+            authors_fullnames = [a['fullname'] for a in db.authors.find()]
+            print(f"Authors in the 'authors' collection: {authors_fullnames}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
-        })
+
